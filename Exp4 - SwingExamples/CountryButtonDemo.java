b61854cdc9732f72e6fc58bdfc420bc4664c8743
Abhishek B <152311913+abhiishek-b@users.noreply.github.com>
2025-05-20 20:03:46 +0530

    Update CountryButtonDemo.java

--- Exp4 - SwingExamples/CountryButtonDemo.java
+++ Exp4 - SwingExamples/CountryButtonDemo.java
@@ -1,5 +1,4 @@
-//4b. Develop a Swing program in Java to display a message “Srilanka is pressed” or “India is
-//pressed” depending upon the Jbutton either Srilanka or India is pressed by implementing the
+//4b. Develop a Swing program in Java to display a message “Srilanka is pressed” or “India is pressed” depending upon the Jbutton either Srilanka or India is pressed by implementing the
 //event handling mechanism with addActionListener( ).
 
 package javaswing;
