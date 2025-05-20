d1325a6bf4926102ef6b2de0d200b276c78fd805
Abhishek B <152311913+abhiishek-b@users.noreply.github.com>
2025-05-20 20:07:33 +0530

    Update Mysql_select.java

--- Exp8 - Database_test _and_insert , update ||JDBC project using JSP/8a.DatabaseTest_and_insert/Mysql_select.java
+++ Exp8 - Database_test _and_insert , update ||JDBC project using JSP/8a.DatabaseTest_and_insert/Mysql_select.java
@@ -1,6 +1,6 @@
-/*8a. Q1. Read all the existing records from the table coffee which is from the database test and insert a
-new coffee product into it [Create a table coffee with fields ( id,coffee_name,price)] using HTML and
-JSP to get the fields and display the results respectively*/
+//8a. Q1. Read all the existing records from the table coffee which is from the database test and insert a
+//new coffee product into it [Create a table coffee with fields ( id,coffee_name,price)] using HTML and
+//JSP to get the fields and display the results respectively
 
 package com.mysql;
 import java.sql.*;
